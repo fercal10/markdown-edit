b25f6ca697c6c8cc3c851d046771f62fddfc3a1a
fercal10 <101475122+fercal10@users.noreply.github.com>
2023-08-21 03:49:43 -0400

    update 7

--- src/components/RenderMarkdown/CustomCode.jsx
+++ src/components/RenderMarkdown/CustomCode.jsx
@@ -1,21 +1,12 @@
 import { useEffect, useState } from 'react';
 import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter'
 import {materialLight, duotoneLight,  vscDarkPlus, dracula} from 'react-syntax-highlighter/dist/esm/styles/prism'
-import useTheme from '../../hooks/useTheme';
 
 function CustomCode({inline, className, children}){
 
-    const [theme] = useTheme();
 
     const [style, setStyle] = useState(undefined);
 
-    useEffect(() => {
-        
-        if(theme === 'dark') setStyle(dracula);
-
-        if(theme === 'light') setStyle(duotoneLight);
-
-    }, [theme]);
 
     //? Expresion regular que comprueba que el className coincida con 'language-**' y extrae el leguaje
     const match = /language-(\w+)/.exec(className || '');
